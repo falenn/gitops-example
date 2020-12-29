7fc1305aed9c47379ecb02b5e1befc0e2d2a3439
falenn <dev@imwiz.com>
2020-12-29 20:34:44 +0000

    added healthz

--- python/app.py
+++ python/app.py
@@ -1,13 +1,28 @@
 #!/bin/env python
 
 from flask import Flask
-
+from flask_healthz import healthz
+from flask_healthz import HealthError
 
 app = Flask(__name__)
+app.register_blueprint(healthz, url_prefix="/healthz")
 
 @app.route("/")
 def hello():
   return "Hello out there!"
+
+def liveness():
+  pass
+
+def readiness():
+  try:
+    True
+  except Exception:
+    raise HealthError("Can't perform function")
+
+
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8000)
+
+
