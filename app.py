1b275317c7b04df74de4e4d975e586332fe92325
Curtis Bates <falenn@users.noreply.github.com>
2020-12-29 14:53:58 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello out there!"
+  
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8000)
