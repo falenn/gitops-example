605c3c1024ec26e2dc20cf82cc87d3ea60d34bef
falenn <dev@imwiz.com>
2020-12-30 20:42:37 +0000

    updated to take in version from workflow.

--- python/webapp/__init__.py
+++ python/webapp/__init__.py
@@ -1,12 +1,17 @@
-#!/bin/env python
-
 from flask import Flask
 from flask_healthz import healthz
+from prometheus_flask_exporter import PrometheusMetrics
+import os
+
 
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(healthz, url_prefix="/healthz")
 app.config.from_envvar("FLASK_CONFIG")
 
+metrics = PrometheusMetrics(app)
+# static information as metric
+VERSION = os.environ['FLASK_APP_VERSION']
+metrics.info('app_info', 'Application info', version=VERSION)
 
 import webapp.views
 import webapp.checks
